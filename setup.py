01ad1e313bf6fb218bfb720e131c6d82d87a4031
gonzalovazquez <gonzalovazquez010@gmail.com>
2020-02-01 00:21:59 -0500

    feat: 🎸 add setup and license file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="open-github-usmakestwo", # Replace with your own username
+    version="0.0.1",
+    author="Gonzalo Vazquez",
+    author_email="gvazquez@usmakestwo.io",
+    description="A simple script that opens a Github page from the command line.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/usmakestwo/open_github",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
